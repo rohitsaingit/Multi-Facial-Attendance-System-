fef7af785156b639a6fa7989eaf6b6251b876e90
satwikshourya <satwikshourya777@gmail.com>
2023-11-18 20:22:18 +0530

    attandance.py newlyadded face_recognition.py updated

--- face_recognition.py
+++ face_recognition.py
@@ -3,6 +3,8 @@ from tkinter import ttk
 from PIL import Image,ImageTk
 from tkinter import messagebox
 import mysql.connector
+from time import strftime
+from datetime import datetime
 import cv2
 import os
 import numpy as np
@@ -38,6 +40,19 @@ class Face_Recognition:
         b1_label = Button(f_1b2, text="Face Recogition", command=self.face_recog,cursor="hand2", font=("times new roman", 18, "bold"), bg="blue", fg="white")
         b1_label.place(x=365, y=620, width=200, height=40)
 
+        # ================ Attandance =======================
+    def mark_attandance(self,i,r,n,d):
+        with open("data.csv", "r+", newline="\n")as f:
+            myDataList=f.readlines()
+            name_list=[]
+            for line in myDataList:
+                entry=line.split((","))
+                name_list.append(entry[0])
+            if((i not in name_list) and (r not in name_list) and (n not in name_list) and (d not in name_list)):
+                now=datetime.now()
+                d1=now.strftime("%d/%m/%Y")
+                dtString=now.strftime("%H:%M:%S")
+                f.writelines(f"\n{i},{r},{n},{d},{dtString},{d1},Present")
 
     # ================ Face Recognition =======================
     def face_recog(self):
@@ -69,10 +84,16 @@ class Face_Recognition:
                 d=my_cursor.fetchone()
                 d="+".join(d)
 
+                my_cursor.execute("select Roll_No from student where Roll_No="+str(id))
+                i=my_cursor.fetchone()
+                i="+".join(i)
+
                 if confidence>77:
+                    cv2.putText(img,f"ID:{i}",(x,y-75),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
                     cv2.putText(img,f"Roll:{r}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
                     cv2.putText(img,f"Name:{n}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
                     cv2.putText(img,f"Dep:{d}",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    self.mark_attandance(i,r,n,d)
                 else:
                     cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
                     cv2.putText(img,"Unknown",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
